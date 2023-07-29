09b7b92e60f255caf2b514aec7f45fec2ddab5b5
Iain Taylor <iain.taylor.94@gmail.com>
2023-07-29 13:38:13 +0000

    completed program

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
+// This program calculates an integer raised to the power of another integer
+
 #include <stdio.h>
 
+// Declare Functions
+long intToInt (int, int);
+
 int main(void) {
-  printf("Hello World\n");
+  // Get input
+  int number, exponent;
+  printf ("Enter input <number> <exponent>: ");
+  scanf ("%d %d",&number, &exponent);
+  
+  // Use program
+  long int answer;
+  answer = intToInt (number, exponent);
+
+  // Print results
+  printf ("The result is:  %ld\n", answer);
+
   return 0;
+}
+
+// Define functions
+long int intToInt (int x, int n) {
+  if (n == 0) {
+    return (1);
+  }
+  else {
+    return (intToInt (x, n-1) * x);
+  }
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Programming in C
+## Chapter 8
+### Question 7
+
+This problem is from chapter 8 of the textbook[^1]. It is to create a program with a function that calculates an intiger raised to the power of another integer.
+
+You may wish to run the program and compare its output.
+
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 162 - Question 7
